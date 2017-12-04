5ecd45fd3ab8e417d2aa3f166c7e1eb74f6913d2
Joris <joris.vigneron@telecomnancy.eu>
2017-12-04 20:10:11 +0100

    ajout version tensorboard + commentaire du code de base

--- projetIA/rdn.py
+++ projetIA/rdn.py
@@ -3,69 +3,69 @@ from __future__ import print_function
 import tensorflow as tf
 
 #Paramètre
-learning_rate = 0.1
-num_steps = 1000
-batch_size = 10
-display_step = 100
+learning_rate = 0.1 	#indice d'apprentissage
+num_steps = 1000    	#nombre d'étape
+batch_size = 10		#taille des batch - inutile
+display_step = 100	#fréquence d'affichage
 
 #Paramètre du réseau
-n_hidden_1 = 64
-n_hidden_2 = 64
-num_input = 1
-num_classes = 2
+n_hidden_1 = 64		#nombre de neurones couche 1
+n_hidden_2 = 64		#nombre de neurones couche 2
+num_input = 32		#nombre d'entrée, à changer en fonction du réseau 
+num_classes = 2		#nombre de sortie attendue toujours 2 => lors de la création du batch de sortie, prévoir 2 colonnes 1 pour chaque sortie
 
 #tf Graph input
-X = tf.placeholder("float",[None,num_input])
-Y = tf.placeholder("float",[None,num_classes])
+X = tf.placeholder("float",[None,num_input])		#configuration de l'ensemble des entrées
+Y = tf.placeholder("float",[None,num_classes])		#configuration de l'ensemble des sorties
 
 #Store layers weight et bias
 weights = {
-    'h1': tf.Variable(tf.random_normal([num_input,n_hidden_1])),
-    'h2': tf.Variable(tf.random_normal([n_hidden_1,n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_hidden_2,num_classes]))
+    'h1': tf.Variable(tf.random_normal([num_input,n_hidden_1])),	#initialisation des poids entrées - couche 1
+    'h2': tf.Variable(tf.random_normal([n_hidden_1,n_hidden_2])),	#initialisation des poids couche 1 - couche 2
+    'out': tf.Variable(tf.random_normal([n_hidden_2,num_classes]))	#initialisation des poids couche 2 - sortie
 }
 
 biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([num_classes]))
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),			#initialisation des biais couche 1
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),			#initialisation des biais couche 2
+    'out': tf.Variable(tf.random_normal([num_classes]))			#initialisation des biais sortie
 }
 
 #Create model
 def neural_net(x):
-    layer_1 = tf.add(tf.matmul(x,weights['h1']),biases['b1'])
-    layer_2 = tf.add(tf.matmul(layer_1,weights['h2']),biases['b2'])
-    out_layer = tf.matmul(layer_2,weights['out'])+biases['out']
+    layer_1 = tf.add(tf.matmul(x,weights['h1']),biases['b1'])		#calcul premiere couche
+    layer_2 = tf.add(tf.matmul(layer_1,weights['h2']),biases['b2'])	#calcul seconde couche
+    out_layer = tf.matmul(layer_2,weights['out'])+biases['out']		#calcul sortie
     return out_layer
 
 
-#construct model
-logits = neural_net(X)
-prediction = tf.nn.softmax(logits)
+#construct modelœ
+logits = neural_net(X)							#création réseau de neurones
+prediction = tf.nn.softmax(logits)					#lissage de la sortie
 
 #define loss and optimizer
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=Y))
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=Y))	#calcul de l'erreur
 #optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-train_op = optimizer.minimize(loss_op)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)			#initialisation descente du gradient
+train_op = optimizer.minimize(loss_op)								#optimisation
 
 #evaluate model
 correct_pred = tf.equal(tf.argmax(prediction,1),tf.argmax(Y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred,tf.float32))
 
 #initialize the variable
-init = tf.global_variables_initializer()
+init = tf.global_variables_initializer()				#initialisation des variables globales
 
 #start training
 with tf.Session() as sess:
-    sess.run(init)
+    sess.run(init)							#run de l'init
 
-    for step in range(1, num_steps+1):
-        batch_x, batch_y = #
+    for step in range(1, num_steps+1):					#boucle d'apprentissage
+        batch_x, batch_y = #						#récupération des données
 
-        sess.run(train_op, feed_dict={X: batch_x, Y:batch_y})
-        if step % display_step == 0 or step = 1:
-            loss, acc = sess.run([loss_op, accuracy], feed_dict={X:batch_x, Y: batch_y})
+        sess.run(train_op, feed_dict={X: batch_x, Y:batch_y})		#lancement de l'apprentissage
+        if step % display_step == 0 or step = 1:			#affichage
+            loss, acc = sess.run([loss_op, accuracy], feed_dict={X:batch_x, Y: batch_y}) 	#calcul de l'erreur
             print("Step " + str(step) + ", Minibatch Loss= " + \
                   "{:.4f}".format(loss) + ", Training Accuracy= " + \
                   "{:.3f}".format(acc))
--- testTensorFlow/TensorFlow-Examples-master/examples/3_NeuralNetworks/neural_network_raw.py
+++ testTensorFlow/TensorFlow-Examples-master/examples/3_NeuralNetworks/neural_network_raw.py
@@ -51,11 +51,11 @@ biases = {
 # Create model
 def neural_net(x):
     # Hidden fully connected layer with 256 neurons
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(tf.add(tf.matmul(x, weights['h1']), biases['b1']))
     # Hidden fully connected layer with 256 neurons
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1, weights['h2']), biases['b2']))
     # Output fully connected layer with a neuron for each class
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    out_layer = tf.nn.relu(tf.matmul(layer_2, weights['out']) + biases['out'])
     return out_layer
 
 # Construct model
